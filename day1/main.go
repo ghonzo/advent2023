39d4df6a764816c2ad6e84ec07673f79ec64086c
Eric Lunt <eric@lunt.software>
2023-11-27 14:33:38 -0600

    Get ready for Day 1

---
+++ day1/main.go
@@ -0,0 +1,26 @@
+// Advent of Code 2023, Day 1
+package main
+
+import (
+	"fmt"
+
+	"github.com/ghonzo/advent2023/common"
+)
+
+// Day 1:
+// Part 1 answer:
+// Part 2 answer:
+func main() {
+	fmt.Println("Advent of Code 2023, Day 1")
+	entries := common.ReadStringsFromFile("input.txt")
+	fmt.Printf("Part 1: %d\n", part1(entries))
+	fmt.Printf("Part 2: %d\n", part2(entries))
+}
+
+func part1(entries []string) int {
+	return 1
+}
+
+func part2(entries []string) int {
+	return 2
+}
