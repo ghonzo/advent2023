b7c1f9c9e38c65e2a300d6f638edbd5ad397e8b5
Eric Lunt <eric@lunt.software>
2023-12-05 19:09:47 -0600

    Day 5 cleanup

--- day5/main.go
+++ day5/main.go
@@ -4,7 +4,6 @@ package main
 import (
 	"fmt"
 	"slices"
-	"sort"
 	"strings"
 
 	"github.com/ghonzo/advent2023/common"
@@ -25,39 +24,43 @@ type seedMap struct {
 }
 
 func part1(entries []string) int {
+	seeds, almanac := parseAlmanac(entries)
+	// Now find the destinations
+	mm := new(common.MaxMin[int])
+	for _, v := range seeds {
+		for _, sms := range almanac {
+			v = doMap(v, sms)
+		}
+		mm.Accept(v)
+	}
+	return mm.Min
+}
+
+// Return the seeds and all of the x-to-y maps
+func parseAlmanac(entries []string) ([]int, [][]seedMap) {
 	// First line is Seeds
 	colonIndex := strings.Index(entries[0], ":")
 	var seeds []int
 	for _, seedStr := range strings.Fields(entries[0][colonIndex+1:]) {
 		seeds = append(seeds, common.Atoi(seedStr))
 	}
-	var allMaps [][]seedMap
+	// The almanac is a collection of all the maps
+	var almanac [][]seedMap
+	// This is just one of the x-to-y maps
 	var sms []seedMap
 	for i := 3; i < len(entries); i++ {
 		line := entries[i]
 		if len(line) == 0 {
-			allMaps = append(allMaps, sms)
+			almanac = append(almanac, sms)
 			sms = make([]seedMap, 0)
 			i++
 		} else {
-			var sm seedMap
 			strs := strings.Fields(line)
-			sm.dest = common.Atoi(strs[0])
-			sm.source = common.Atoi(strs[1])
-			sm.length = common.Atoi(strs[2])
-			sms = append(sms, sm)
+			sms = append(sms, seedMap{dest: common.Atoi(strs[0]), source: common.Atoi(strs[1]), length: common.Atoi(strs[2])})
 		}
 	}
-	allMaps = append(allMaps, sms)
-	// Now find the destinations
-	mm := new(common.MaxMin[int])
-	for _, v := range seeds {
-		for _, sms := range allMaps {
-			v = doMap(v, sms)
-		}
-		mm.Accept(v)
-	}
-	return mm.Min
+	almanac = append(almanac, sms)
+	return seeds, almanac
 }
 
 func doMap(v int, sms []seedMap) int {
@@ -72,52 +75,19 @@ func doMap(v int, sms []seedMap) int {
 }
 
 func part2(entries []string) int {
-	// First line is Seeds
-	colonIndex := strings.Index(entries[0], ":")
-	var seeds []int
-	for _, seedStr := range strings.Fields(entries[0][colonIndex+1:]) {
-		seeds = append(seeds, common.Atoi(seedStr))
-	}
-	var allMaps [][]seedMap
-	var sms []seedMap
-	for i := 3; i < len(entries); i++ {
-		line := entries[i]
-		if len(line) == 0 {
-			allMaps = append(allMaps, sms)
-			sms = make([]seedMap, 0)
-			i++
-		} else {
-			var sm seedMap
-			strs := strings.Fields(line)
-			sm.dest = common.Atoi(strs[0])
-			sm.source = common.Atoi(strs[1])
-			sm.length = common.Atoi(strs[2])
-			sms = append(sms, sm)
-		}
-	}
-	allMaps = append(allMaps, sms)
+	seeds, almanac := parseAlmanac(entries)
 	// Now reverse the maps
-	slices.Reverse(allMaps)
-	// Now start with the destinations and go backwards until we find seeds
-	locationMap := allMaps[0]
-	sort.Slice(locationMap, func(i, j int) bool {
-		return locationMap[i].dest < locationMap[j].dest
-	})
-	locationMap = append([]seedMap{{dest: 0, source: 0, length: locationMap[0].dest}}, locationMap...)
-	for _, sm := range locationMap {
-		// Do the backwards map until we find a valid seed
-		for location := sm.dest; location < sm.dest+sm.length; location++ {
-			v := location
-			for _, sms := range allMaps[1:] {
-				v = doReverseMap(v, sms)
-			}
-			if validSeed(v, seeds) {
-				return location
-			}
-
+	slices.Reverse(almanac)
+	// Just start counting up from 0 and do reverse map until we get to a valid seed
+	for location := 0; ; location++ {
+		v := location
+		for _, sms := range almanac[1:] {
+			v = doReverseMap(v, sms)
+		}
+		if validSeed(v, seeds) {
+			return location
 		}
 	}
-	panic("derp")
 }
 
 func doReverseMap(v int, sms []seedMap) int {
