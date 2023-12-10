aef5c33175d664f933ec583ac45af05f7dc85f80
Eric Lunt <eric@lunt.software>
2023-12-10 12:09:57 -0600

    Day 10 cleanup

--- day10/main.go
+++ day10/main.go
@@ -7,9 +7,9 @@ import (
 	"github.com/ghonzo/advent2023/common"
 )
 
-// Day 10:
+// Day 10: Pipe Maze
 // Part 1 answer: 6773
-// Part 2 answer:
+// Part 2 answer: 493
 func main() {
 	fmt.Println("Advent of Code 2023, Day 10")
 	lines := common.ReadStringsFromFile("input.txt")
@@ -19,76 +19,79 @@ func main() {
 
 func part1(lines []string) int {
 	grid := common.ArraysGridFromLines(lines)
-	// Find starting point
-	var startingPoint common.Point
-	for startingPoint = range grid.AllPoints() {
-		if grid.Get(startingPoint) == 'S' {
-			break
-		}
-	}
-	// First, figure out which direction we can follow from the starting position
-	lastDirection, nextDirection := findFirstDirections(grid, startingPoint)
-	// Okay now we can just starting following it and remembering the points
-	currentPoint := startingPoint.Add(lastDirection)
-	var allPoints []common.Point
-	for currentPoint != startingPoint {
-		allPoints = append(allPoints, currentPoint)
-		currentPoint = currentPoint.Add(nextDirection)
-		nextDirection = findNextDirection(grid.Get(currentPoint), nextDirection)
-	}
-	return (len(allPoints) + 1) / 2
+	startingPoint := findStartingPoint(grid)
+	// Not strictly necessary, but helps us down the road
+	grid.Set(startingPoint, determineActualTile(grid, startingPoint))
+	pathPoints := findPath(grid, startingPoint)
+	return len(pathPoints) / 2
 }
 
-func findFirstDirections(g common.Grid, startingPoint common.Point) (lastDirection common.Point, nextDirection common.Point) {
-	lastDirection = common.N
-	p := startingPoint.Add(lastDirection)
-	v, ok := g.CheckedGet(p)
-	if ok {
-		switch v {
-		case '|':
-			nextDirection = common.N
-			return
-		case '7':
-			nextDirection = common.W
-			return
-		case 'F':
-			nextDirection = common.E
-			return
+func findStartingPoint(g common.Grid) common.Point {
+	for p := range g.AllPoints() {
+		if g.Get(p) == 'S' {
+			return p
 		}
 	}
-	lastDirection = common.E
-	p = startingPoint.Add(lastDirection)
-	v, ok = g.CheckedGet(p)
-	if ok {
-		switch v {
-		case '-':
-			nextDirection = common.E
-			return
-		case 'J':
-			nextDirection = common.N
-			return
-		case '7':
-			nextDirection = common.S
-			return
-		}
+	panic("no starting point")
+}
+
+func determineActualTile(g common.Grid, startingPoint common.Point) byte {
+	// Set of directions that feed into this space
+	cardinalMap := make(map[common.Point]bool)
+	// North first
+	direction := common.N
+	if v, ok := g.CheckedGet(startingPoint.Add(direction)); ok {
+		cardinalMap[direction] = (v == '|' || v == '7' || v == 'F')
 	}
-	lastDirection = common.S
-	p = startingPoint.Add(lastDirection)
-	v, ok = g.CheckedGet(p)
-	if ok {
-		switch v {
-		case '|':
-			nextDirection = common.S
-			return
-		case 'J':
-			nextDirection = common.W
-			return
-		case 'L':
-			nextDirection = common.E
-			return
-		}
+	direction = common.E
+	if v, ok := g.CheckedGet(startingPoint.Add(direction)); ok {
+		cardinalMap[direction] = (v == '-' || v == 'J' || v == '7')
 	}
-	panic("nope")
+	direction = common.S
+	if v, ok := g.CheckedGet(startingPoint.Add(direction)); ok {
+		cardinalMap[direction] = (v == '|' || v == 'L' || v == 'J')
+	}
+	// West can be derived
+	var actualTile byte
+	if cardinalMap[common.N] {
+		if cardinalMap[common.E] {
+			actualTile = 'L'
+		} else if cardinalMap[common.S] {
+			actualTile = '|'
+		} else {
+			actualTile = 'J'
+		}
+	} else if cardinalMap[common.E] {
+		if cardinalMap[common.S] {
+			actualTile = 'F'
+		} else {
+			actualTile = '-'
+		}
+	} else {
+		actualTile = '7'
+	}
+	return actualTile
+}
+
+func findPath(g common.Grid, startingPoint common.Point) map[common.Point]bool {
+	pathPoints := make(map[common.Point]bool)
+	// Which direction should we start with?
+	var direction common.Point
+	switch g.Get(startingPoint) {
+	case '|', 'L', 'J':
+		direction = common.N
+	case '-', 'F':
+		direction = common.E
+	case '7':
+		direction = common.S
+	}
+	currentPoint := startingPoint
+	for !pathPoints[currentPoint] {
+		pathPoints[currentPoint] = true
+		currentPoint = currentPoint.Add(direction)
+		direction = findNextDirection(g.Get(currentPoint), direction)
+	}
+	return pathPoints
 }
 
 func findNextDirection(tile byte, lastDir common.Point) common.Point {
@@ -130,75 +133,24 @@ func findNextDirection(tile byte, lastDir common.Point) common.Point {
 			return common.S
 		}
 	}
-	return common.Point{}
+	panic("invalid path")
 }
 
 func part2(lines []string) int {
-	// First, we need to replace the "S" with the appropirate other shape
 	grid := common.ArraysGridFromLines(lines)
-	// Find starting point
-	var startingPoint common.Point
-	for startingPoint = range grid.AllPoints() {
-		if grid.Get(startingPoint) == 'S' {
-			break
-		}
-	}
-	// Find which directions feed into this space
-	cardinalMap := make(map[common.Point]bool)
-	// North first
-	direction := common.N
-	if v, ok := grid.CheckedGet(startingPoint.Add(direction)); ok {
-		cardinalMap[direction] = (v == '|' || v == '7' || v == 'F')
-	}
-	direction = common.E
-	if v, ok := grid.CheckedGet(startingPoint.Add(direction)); ok {
-		cardinalMap[direction] = (v == '-' || v == 'J' || v == '7')
-	}
-	direction = common.S
-	if v, ok := grid.CheckedGet(startingPoint.Add(direction)); ok {
-		cardinalMap[direction] = (v == '|' || v == 'L' || v == 'J')
-	}
-	// West can be derived
-	var actualShape byte
-	if cardinalMap[common.N] {
-		if cardinalMap[common.E] {
-			actualShape = 'L'
-		} else if cardinalMap[common.S] {
-			actualShape = '|'
-		} else {
-			actualShape = 'J'
-		}
-	} else if cardinalMap[common.E] {
-		if cardinalMap[common.S] {
-			actualShape = 'F'
-		} else {
-			actualShape = '-'
-		}
-	} else {
-		actualShape = '7'
-	}
-	// Now replace it in the grid
-	grid.Set(startingPoint, actualShape)
-	// Put all the points of the path in a map
-	pathPoints := make(map[common.Point]bool)
-	pathPoints[startingPoint] = true
-	lastDirection, nextDirection := findFirstDirections(grid, startingPoint)
-	// Okay now we can just starting following it and remembering the points
-	currentPoint := startingPoint.Add(lastDirection)
-	for currentPoint != startingPoint {
-		pathPoints[currentPoint] = true
-		currentPoint = currentPoint.Add(nextDirection)
-		nextDirection = findNextDirection(grid.Get(currentPoint), nextDirection)
-	}
+	startingPoint := findStartingPoint(grid)
+	// Not strictly necessary, but helps us down the road
+	grid.Set(startingPoint, determineActualTile(grid, startingPoint))
+	pathPoints := findPath(grid, startingPoint)
 	// Now we need to walk East from the left side and find the number of intersections
 	// Fortunately, that's the way AllPoints() returns them
 	interior := false
 	var interiorPoints int
 	var lastElbow byte
 	for p := range grid.AllPoints() {
-		v := grid.Get(p)
 		// Is it part of the path?
 		if pathPoints[p] {
+			v := grid.Get(p)
 			switch v {
 			case '|':
 				interior = !interior
@@ -208,12 +160,12 @@ func part2(lines []string) int {
 				if lastElbow == 'F' {
 					interior = !interior
 				}
-				lastElbow = 0
+				lastElbow = v
 			case '7':
 				if lastElbow == 'L' {
 					interior = !interior
 				}
-				lastElbow = 0
+				lastElbow = v
 			}
 		} else if interior {
 			interiorPoints++
