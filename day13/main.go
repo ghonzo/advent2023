67d2082e279a5fd60f3fd2031e0ce6858e1b5f08
Eric Lunt <eric@lunt.software>
2023-12-13 22:56:16 -0600

    Day 13 (not cleaned)

--- day13/main.go
+++ day13/main.go
@@ -10,7 +10,7 @@ import (
 
 // Day 13: Point of Incidence
 // Part 1 answer: 34772
-// Part 2 answer:
+// Part 2 answer: 35554
 func main() {
 	fmt.Println("Advent of Code 2023, Day 13")
 	lines := common.ReadStringsFromFile("input.txt")
@@ -23,15 +23,14 @@ func part1(lines []string) int {
 	lastBlank := -1
 	for n, line := range append(lines, "") {
 		if len(line) == 0 {
-			total += scoreForPattern(lines[lastBlank+1 : n])
+			total += scoreForPattern(common.ArraysGridFromLines(lines[lastBlank+1:n]), 0)
 			lastBlank = n
 		}
 	}
 	return total
 }
 
-func scoreForPattern(lines []string) int {
-	g := common.ArraysGridFromLines(lines)
+func scoreForPattern(g common.Grid, omitScore int) int {
 	// Score -> sum of column
 	colScore := make([]uint64, g.Size().X())
 	for x := 0; x < g.Size().X(); x++ {
@@ -44,7 +43,7 @@ func scoreForPattern(lines []string) int {
 		}
 		colScore[x] = score
 	}
-	if col, ok := findSymmetry(colScore); ok {
+	if col, ok := findSymmetry(colScore, omitScore%100); ok {
 		return col
 	}
 	// Let's look for rows
@@ -59,14 +58,17 @@ func scoreForPattern(lines []string) int {
 		}
 		rowScore[y] = score
 	}
-	if row, ok := findSymmetry(rowScore); ok {
+	if row, ok := findSymmetry(rowScore, omitScore/100); ok {
 		return row * 100
 	}
-	panic("none")
+	return 0
 }
 
-func findSymmetry(scores []uint64) (int, bool) {
+func findSymmetry(scores []uint64, omit int) (int, bool) {
 	for i := 0; i < len(scores)-1; i++ {
+		if i == omit-1 {
+			continue
+		}
 		size := min(i+1, len(scores)-i-1)
 		leftSlice := scores[i+1-size : i+1]
 		rightSlice := scores[i+1 : i+1+size]
@@ -81,5 +83,32 @@ func findSymmetry(scores []uint64) (int, bool) {
 
 func part2(lines []string) int {
 	var total int
+	lastBlank := -1
+	for n, line := range append(lines, "") {
+		if len(line) == 0 {
+			total += scoreForPattern2(common.ArraysGridFromLines(lines[lastBlank+1 : n]))
+			lastBlank = n
+		}
+	}
 	return total
+}
+
+func scoreForPattern2(g common.Grid) int {
+	// First figure out the original score
+	originalScore := scoreForPattern(g, 0)
+	// Now brute force variations
+	for p := range g.AllPoints() {
+		v := g.Get(p)
+		if v == '.' {
+			g.Set(p, '#')
+		} else {
+			g.Set(p, '.')
+		}
+		newScore := scoreForPattern(g, originalScore)
+		if newScore > 0 {
+			return newScore
+		}
+		g.Set(p, v)
+	}
+	panic("none")
 }
