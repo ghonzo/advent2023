fe23c64fd00f6becd81bbadcf86a0858bc0acc01
Eric Lunt <eric@lunt.software>
2023-12-15 23:37:29 -0600

    Day 16

---
+++ day16/main.go
@@ -0,0 +1,102 @@
+// Advent of Code 2023, Day 16
+package main
+
+import (
+	"fmt"
+
+	"github.com/ghonzo/advent2023/common"
+)
+
+// Day 16: The Floor Will Be Lava
+// Part 1 answer: 8551
+// Part 2 answer: 8754
+func main() {
+	fmt.Println("Advent of Code 2023, Day 16")
+	lines := common.ReadStringsFromFile("input.txt")
+	fmt.Printf("Part 1: %d\n", part1(lines))
+	fmt.Printf("Part 2: %d\n", part2(lines))
+}
+
+type beam struct {
+	pos common.Point
+	dir common.Point
+}
+
+func part1(lines []string) int {
+	g := common.ArraysGridFromLines(lines)
+	return calcEnergized(g, beam{pos: common.NewPoint(-1, 0), dir: common.R})
+}
+
+func calcEnergized(g common.Grid, initialBeam beam) int {
+	energized := make(map[common.Point]bool)
+	beamsSeen := make(map[beam]bool)
+	beams := []beam{initialBeam}
+	for len(beams) > 0 {
+		// Pop off the top
+		b := beams[0]
+		beams = beams[1:]
+		// Move
+		b.pos = b.pos.Add(b.dir)
+		v, ok := g.CheckedGet(b.pos)
+		if !ok || beamsSeen[b] {
+			continue
+		}
+		energized[b.pos] = true
+		beamsSeen[b] = true
+		switch v {
+		case '.':
+			beams = append(beams, b)
+		case '/':
+			switch b.dir {
+			case common.U:
+				b.dir = common.R
+			case common.R:
+				b.dir = common.U
+			case common.D:
+				b.dir = common.L
+			case common.L:
+				b.dir = common.D
+			}
+			beams = append(beams, b)
+		case '\\':
+			switch b.dir {
+			case common.U:
+				b.dir = common.L
+			case common.R:
+				b.dir = common.D
+			case common.D:
+				b.dir = common.R
+			case common.L:
+				b.dir = common.U
+			}
+			beams = append(beams, b)
+		case '|':
+			if b.dir == common.R || b.dir == common.L {
+				beams = append(beams, beam{pos: b.pos, dir: common.U}, beam{pos: b.pos, dir: common.D})
+			} else {
+				beams = append(beams, b)
+			}
+		case '-':
+			if b.dir == common.U || b.dir == common.D {
+				beams = append(beams, beam{pos: b.pos, dir: common.R}, beam{pos: b.pos, dir: common.L})
+			} else {
+				beams = append(beams, b)
+			}
+		}
+	}
+	return len(energized)
+}
+
+func part2(lines []string) int {
+	g := common.ArraysGridFromLines(lines)
+	var maxEnergized int
+	for x := 0; x < g.Size().X(); x++ {
+		maxEnergized = max(maxEnergized, calcEnergized(g, beam{pos: common.NewPoint(x, -1), dir: common.D}),
+			calcEnergized(g, beam{pos: common.NewPoint(x, g.Size().Y()), dir: common.U}))
+	}
+	for y := 0; y < g.Size().Y(); y++ {
+		maxEnergized = max(maxEnergized, calcEnergized(g, beam{pos: common.NewPoint(-1, y), dir: common.R}),
+			calcEnergized(g, beam{pos: common.NewPoint(g.Size().X(), y), dir: common.L}))
+	}
+	return maxEnergized
+}
