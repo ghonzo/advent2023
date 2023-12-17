428026517c7f81725a9d2574dda2c4048a8f998a
Eric Lunt <eric@lunt.software>
2023-12-17 11:45:42 -0600

    Day 17 cleanup

--- day17/main.go
+++ day17/main.go
@@ -14,196 +14,117 @@ import (
 func main() {
 	fmt.Println("Advent of Code 2023, Day 17")
 	lines := common.ReadStringsFromFile("input.txt")
-	//lines := common.ReadStringsFromFile("testdata/example.txt")
 	fmt.Printf("Part 1: %d\n", part1(lines))
 	fmt.Printf("Part 2: %d\n", part2(lines))
 }
 
 type state struct {
-	pos      common.Point
-	prevDir  []common.Point
-	heatLoss int
+	pos     common.Point
+	prevDir common.Point
+	// Number of times we've move this direction
+	times int
 }
 
-func (s state) validNextDirections() []common.Point {
-	var nextDirections []common.Point
-	skipDirection := determineSkipDirection(s.prevDir)
-	for _, dir := range []common.Point{common.R, common.D, common.L, common.U} {
-		if dir == skipDirection {
-			continue
-		}
-		// Also can't go back the way we came
-		if len(s.prevDir) > 0 && dir == s.prevDir[len(s.prevDir)-1].Reflect() {
-			continue
-		}
-		nextDirections = append(nextDirections, dir)
-	}
-	return nextDirections
-}
+type continueFlag int
+
+const (
+	FINISHED continueFlag = iota
+	ABORT
+	CONTINUE
+)
 
 func part1(lines []string) int {
 	g := common.ArraysGridFromLines(lines)
-	return findShortestPath(g)
-}
-
-type memoState struct {
-	pos                common.Point
-	lastDirectionsHash uint64
-}
-
-var dirHash = map[common.Point]uint64{common.U: 1, common.R: 2, common.D: 4, common.L: 8}
-
-func (s state) memo() memoState {
-	var h uint64
-	size := len(s.prevDir)
-	for i := 0; i < min(size, 3); i++ {
-		h *= 16
-		h += dirHash[s.prevDir[size-i-1]]
-	}
-	return memoState{s.pos, h}
-}
-
-func (s state) memo2() memoState {
-	var h uint64
-	size := len(s.prevDir)
-	for i := 0; i < min(size, 10); i++ {
-		h *= 16
-		h += dirHash[s.prevDir[size-i-1]]
-	}
-	return memoState{s.pos, h}
+	finishPoint := g.Size().Add(common.NW)
+	return findShortestPath(g, func(s state) continueFlag {
+		if s.pos == finishPoint {
+			return FINISHED
+		} else {
+			return CONTINUE
+		}
+	}, func(s state) []common.Point {
+		var nextDirections []common.Point
+		for _, dir := range []common.Point{common.R, common.D, common.L, common.U} {
+			if dir == s.prevDir.Reflect() {
+				continue
+			}
+			if dir == s.prevDir && s.times == 3 {
+				continue
+			}
+			nextDirections = append(nextDirections, dir)
+		}
+		return nextDirections
+	})
 }
 
 // Return minimum heat loss
-func findShortestPath(g common.Grid) int {
-	finishPoint := g.Size().Add(common.NW)
+func findShortestPath(g common.Grid, completeFn func(s state) continueFlag, nextMovesFn func(s state) []common.Point) int {
 	// Stores the minimum heat loss we've seen at each point
-	minHeatLoss := make(map[memoState]int)
+	minHeatLoss := make(map[state]int)
+	// State, with accumulated heat loss as our scoring function
 	pq := lane.NewMinPriorityQueue[state, int]()
 	pq.Push(state{}, 0)
 	for !pq.Empty() {
-		curState, _, _ := pq.Pop()
-		// Did we finish?
-		if curState.pos == finishPoint {
-			//printSolution(g, curState)
-			return curState.heatLoss
+		curState, heatLoss, _ := pq.Pop()
+		cf := completeFn(curState)
+		if cf == FINISHED {
+			return heatLoss
 		}
-		ms := curState.memo()
+		if cf == ABORT {
+			continue
+		}
 		// Have we been here before?
-		if mhl, ok := minHeatLoss[ms]; ok && curState.heatLoss >= mhl {
+		if mhl, ok := minHeatLoss[curState]; ok && heatLoss >= mhl {
 			// Yes, so we can forget this path
 			continue
 		}
 		// Remember we've been here
-		minHeatLoss[ms] = curState.heatLoss
-		// Now find all the next moves, but skip a direction if we've used it the last three times
-		for _, dir := range curState.validNextDirections() {
+		minHeatLoss[curState] = heatLoss
+		// Now find all the next moves
+		for _, dir := range nextMovesFn(curState) {
 			np := curState.pos.Add(dir)
 			if heatLossChar, ok := g.CheckedGet(np); ok {
-				newHeatLoss := curState.heatLoss + int(heatLossChar-'0')
-				newState := state{pos: np, heatLoss: newHeatLoss}
-				newState.prevDir = append(newState.prevDir, curState.prevDir...)
-				newState.prevDir = append(newState.prevDir, dir)
-				pq.Push(newState, newHeatLoss)
+				newHeatLoss := int(heatLossChar - '0')
+				newState := state{pos: np}
+				if curState.prevDir == dir {
+					newState.times = curState.times + 1
+				} else {
+					newState.times = 1
+				}
+				newState.prevDir = dir
+				pq.Push(newState, heatLoss+newHeatLoss)
 			}
 		}
 	}
 	panic("rats")
-}
-
-func determineSkipDirection(prevDir []common.Point) common.Point {
-	size := len(prevDir)
-	if size >= 3 && prevDir[size-1] == prevDir[size-2] && prevDir[size-1] == prevDir[size-3] {
-		return prevDir[size-1]
-	}
-	return common.Point{}
-}
-
-var dirChars = map[common.Point]byte{common.U: '^', common.R: '>', common.D: 'v', common.L: '<'}
-
-func printSolution(g common.Grid, s state) {
-	var p common.Point
-	for _, dir := range s.prevDir {
-		p = p.Add(dir)
-		g.Set(p, dirChars[dir])
-	}
-	fmt.Println(common.RenderGrid(g))
 }
 
 func part2(lines []string) int {
 	g := common.ArraysGridFromLines(lines)
-	return findShortestPath2(g)
-}
-
-func findShortestPath2(g common.Grid) int {
 	finishPoint := g.Size().Add(common.NW)
-	// Stores the minimum heat loss we've seen at each point
-	minHeatLoss := make(map[memoState]int)
-	pq := lane.NewMinPriorityQueue[state, int]()
-	pq.Push(state{}, 0)
-	for !pq.Empty() {
-		curState, _, _ := pq.Pop()
-		// Did we finish?
-		if curState.pos == finishPoint {
-			if curState.timesMostRecent() < 4 {
+	return findShortestPath(g, func(s state) continueFlag {
+		if s.pos == finishPoint {
+			if s.times < 4 {
+				return ABORT
+			} else {
+				return FINISHED
+			}
+		}
+		return CONTINUE
+	}, func(s state) []common.Point {
+		if s.times > 0 && s.times < 4 {
+			return []common.Point{s.prevDir}
+		}
+		var nextDirections []common.Point
+		for _, dir := range []common.Point{common.R, common.D, common.L, common.U} {
+			if dir == s.prevDir.Reflect() {
 				continue
 			}
-			return curState.heatLoss
-		}
-		ms := curState.memo2()
-		// Have we been here before?
-		if mhl, ok := minHeatLoss[ms]; ok && curState.heatLoss >= mhl {
-			// Yes, so we can forget this path
-			continue
-		}
-		// Remember we've been here
-		minHeatLoss[ms] = curState.heatLoss
-		// Now find all the next moves, but skip a direction if we've used it the last three times
-		for _, dir := range curState.validNextDirections2() {
-			np := curState.pos.Add(dir)
-			if heatLossChar, ok := g.CheckedGet(np); ok {
-				newHeatLoss := curState.heatLoss + int(heatLossChar-'0')
-				newState := state{pos: np, heatLoss: newHeatLoss}
-				newState.prevDir = append(newState.prevDir, curState.prevDir...)
-				newState.prevDir = append(newState.prevDir, dir)
-				pq.Push(newState, newHeatLoss)
+			if dir == s.prevDir && s.times == 10 {
+				continue
 			}
-		}
-	}
-	panic("rats")
-}
-
-func (s state) timesMostRecent() int {
-	size := len(s.prevDir)
-	if size == 0 {
-		return 0
-	}
-	mostRecentDirection := s.prevDir[size-1]
-	var times = 1
-	for ; times < size && mostRecentDirection == s.prevDir[size-times-1]; times++ {
-	}
-	return times
-}
-
-func (s state) validNextDirections2() []common.Point {
-	size := len(s.prevDir)
-	if size == 0 {
-		return []common.Point{common.R, common.D}
-	}
-	// Figure out the number of times we have gone the most recent direction
-	mostRecentDirection := s.prevDir[size-1]
-	var times = s.timesMostRecent()
-	if times < 4 {
-		// Gotta keep going
-		return []common.Point{mostRecentDirection}
-	}
-	var nextDirections []common.Point
-	for _, dir := range []common.Point{common.R, common.D, common.L, common.U} {
-		if times == 10 && dir == mostRecentDirection {
-			continue
-		}
-		if dir != mostRecentDirection.Reflect() {
 			nextDirections = append(nextDirections, dir)
 		}
-	}
-	return nextDirections
+		return nextDirections
+	})
 }
