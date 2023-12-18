2f6a1a9e1787eadb34cece50af7ae4732030d3cb
Eric Lunt <eric@lunt.software>
2023-12-18 01:22:56 -0600

    Day 18 cleanup

--- day18/main.go
+++ day18/main.go
@@ -9,7 +9,7 @@ import (
 	"github.com/ghonzo/advent2023/common"
 )
 
-// Day 18:
+// Day 18: Lavaduct Lagoon
 // Part 1 answer: 62365
 // Part 2 answer: 159485361249806
 func main() {
@@ -22,83 +22,24 @@ func main() {
 var dirMap = map[byte]common.Point{'U': common.U, 'R': common.R, 'D': common.D, 'L': common.L}
 
 func part1(lines []string) int {
-	g := common.NewSparseGrid()
+	var vertices []common.Point
 	var p common.Point
-	// Set the trench
+	var total int
 	for _, line := range lines {
 		parts := strings.Fields(line)
 		dir := dirMap[parts[0][0]]
-		for i := 0; i < common.Atoi(parts[1]); i++ {
-			g.Set(p, '#')
-			p = p.Add(dir)
-		}
+		length := common.Atoi(parts[1])
+		vertices = append(vertices, p)
+		// Edges count!
+		total += int(length)
+		p = p.Add(dir.Times(int(length)))
 	}
-	return calculateArea(g)
-}
-
-func calculateArea(g common.Grid) int {
-	// Now calculate the contained area. Start by figuring out the bounds
-	mmX := new(common.MaxMin[int])
-	mmY := new(common.MaxMin[int])
-	for p := range g.AllPoints() {
-		mmX.Accept(p.X())
-		mmY.Accept(p.Y())
+	// Trapezoid forumula for the area of a polygon
+	for i := 0; i < len(vertices); i++ {
+		j := (i + 1) % len(vertices)
+		total += (vertices[i].Y() + vertices[j].Y()) * (vertices[i].X() - vertices[j].X())
 	}
-	var total int
-	for y := mmY.Min; y <= mmY.Max; y++ {
-		interior := false
-		var lastElbow byte
-		for x := mmX.Min; x <= mmX.Max; x++ {
-			spaceType := determineSpaceType(g, x, y)
-			switch spaceType {
-			case '|':
-				interior = !interior
-			case 'F', 'L':
-				lastElbow = spaceType
-			case 'J':
-				if lastElbow == 'F' {
-					interior = !interior
-				}
-				lastElbow = spaceType
-			case '7':
-				if lastElbow == 'L' {
-					interior = !interior
-				}
-				lastElbow = spaceType
-			}
-			if spaceType != '.' || interior {
-				total++
-			}
-		}
-	}
-	return total
-}
-
-func present(g common.Grid, p common.Point) bool {
-	v, ok := g.CheckedGet(p)
-	return ok && v != 0
-}
-
-func determineSpaceType(g common.Grid, x, y int) byte {
-	p := common.NewPoint(x, y)
-	if !present(g, p) {
-		return '.'
-	}
-	switch {
-	case !present(g, p.Add(common.L)) && !present(g, p.Add(common.R)):
-		return '|'
-	case present(g, p.Add(common.L)) && present(g, p.Add(common.R)):
-		return '-'
-	case present(g, p.Add(common.R)) && present(g, p.Add(common.D)):
-		return 'F'
-	case present(g, p.Add(common.L)) && present(g, p.Add(common.D)):
-		return '7'
-	case present(g, p.Add(common.L)) && present(g, p.Add(common.U)):
-		return 'J'
-	case present(g, p.Add(common.R)) && present(g, p.Add(common.U)):
-		return 'L'
-	}
-	panic("ugh")
+	return total/2 + 1
 }
 
 var dirMap2 = map[byte]common.Point{'0': common.R, '1': common.D, '2': common.L, '3': common.U}
@@ -116,6 +57,7 @@ func part2(lines []string) int {
 		total += int(length)
 		p = p.Add(dir.Times(int(length)))
 	}
+	// Trapezoid forumula for the area of a polygon
 	for i := 0; i < len(vertices); i++ {
 		j := (i + 1) % len(vertices)
 		total += (vertices[i].Y() + vertices[j].Y()) * (vertices[i].X() - vertices[j].X())
