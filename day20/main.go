49996c0ef291333239a87d4cc6baaf532ef746a0
Eric Lunt <eric@lunt.software>
2023-12-20 00:22:11 -0600

    Day 20 part 2 brute force

--- day20/main.go
+++ day20/main.go
@@ -127,5 +127,76 @@ func pressButton(moduleDefMap map[string]moduleDef, moduleStateMap map[string]*m
 }
 
 func part2(lines []string) int {
-	return 0
+	moduleDefMap := make(map[string]moduleDef)
+	for _, line := range lines {
+		var md moduleDef
+		parts := strings.Split(line, " -> ")
+		if parts[0] == "broadcaster" {
+			md.name = parts[0]
+		} else {
+			md.name = parts[0][1:]
+			if parts[0][0] == '&' {
+				md.conjunction = true
+			}
+		}
+		md.outputs = strings.Split(parts[1], ", ")
+		moduleDefMap[md.name] = md
+	}
+	// Now create all the moduleStates
+	moduleStateMap := make(map[string]*moduleState)
+	for _, md := range moduleDefMap {
+		moduleStateMap[md.name] = &moduleState{conInputs: make(map[string]bool)}
+	}
+	// Now link all the conjunction outputs
+	for _, md := range moduleDefMap {
+		for _, otherMd := range md.outputs {
+			if ms, ok := moduleStateMap[otherMd]; ok {
+				ms.conInputs[md.name] = false
+			}
+		}
+	}
+	for i := 0; ; i++ {
+		if pressButton2(moduleDefMap, moduleStateMap) {
+			return i + 1
+		}
+	}
+}
+
+func pressButton2(moduleDefMap map[string]moduleDef, moduleStateMap map[string]*moduleState) bool {
+	// Button sends low to broadcaster
+	var pulses []pulse
+	// Do the broadcaster first
+	for _, output := range moduleDefMap["broadcaster"].outputs {
+		pulses = append(pulses, pulse{origin: "broadcaster", dest: output})
+	}
+	for len(pulses) > 0 {
+		p := pulses[0]
+		if p.dest == "rx" && !p.high {
+			return true
+		}
+		pulses = pulses[1:]
+		md, ok := moduleDefMap[p.dest]
+		if !ok {
+			continue
+		}
+		ms := moduleStateMap[p.dest]
+		var outputHigh bool
+		if md.conjunction {
+			// Record what we received
+			ms.conInputs[p.origin] = p.high
+			outputHigh = !ms.allHigh()
+		} else if p.high {
+			// A flip-flop module received a high pulse, which means nothing happens
+			continue
+		} else {
+			// A flip-flop module received a low pulse
+			ms.flipFlop = !ms.flipFlop
+			outputHigh = ms.flipFlop
+		}
+		// Send it to all outputs
+		for _, output := range md.outputs {
+			pulses = append(pulses, pulse{origin: md.name, dest: output, high: outputHigh})
+		}
+	}
+	return false
 }
